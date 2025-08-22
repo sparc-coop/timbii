e95cfefb11c989614660e70de6c03dad44a65e65
Joseph Young <joseph@josephayoung.com>
2025-08-22 07:36:43 -0500

    Switch Timbii to WASM

--- Timbii/Program.cs
+++ Timbii/Program.cs
@@ -1,7 +1,8 @@
 using Sparc.Blossom.Engine;
+using Timbii.Shared;
 
-var builder = BlossomApplication.CreateBuilder<Html>(args);
+var builder = BlossomApplication.CreateBuilder<BlossomApp<Program, MainLayout>>(args);
 builder.Services.AddBlossomEngine("https://localhost:7185");
 var app = builder.Build();
 
-await app.RunAsync<Html>();
+await app.RunAsync<BlossomApp<Program, MainLayout>>();
