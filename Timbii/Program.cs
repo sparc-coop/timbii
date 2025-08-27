7d46de24f7671a7831a84a1720c2027a5b339d53
Joseph Young <joseph@josephayoung.com>
2025-08-27 11:34:05 -0500

    Working automatic initialization of Dexie db!

--- Timbii/Program.cs
+++ Timbii/Program.cs
@@ -3,6 +3,7 @@ using Timbii.Shared;
 
 var builder = BlossomApplication.CreateBuilder<BlossomApp<Program, MainLayout>>(args);
 builder.Services.AddBlossomEngine("https://localhost:7185");
+builder.Services.AddDexie<Room>();
 var app = builder.Build();
 
 await app.RunAsync<BlossomApp<Program, MainLayout>>();
